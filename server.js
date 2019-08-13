7b5a1e18e65ac256324a6942236618aee9240c5e
Wesley Shih <shih.wesley@gmail.com>
2019-08-13 16:27:26 -0700

    uploading friend finder HW

--- server.js
+++ server.js
@@ -1,23 +1,40 @@
-// Dependencies
-// ===========================================================
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
 var express = require("express");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
 
-// Sets up the Express app to handle data parsing
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// express.json and express.urlEncoded make it easy for our server to interpret data sent to it.
+// The code below is pretty standard.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//data
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
-//routes
-const routes = require('./routes');
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
-app.use(routes);
+// ==============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// ==============================================================================
 
-
-// =============================================================
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
